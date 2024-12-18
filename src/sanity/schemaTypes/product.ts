6c7fd329884a5651595eeaba160949e156fb7dc2
Hamza Shabir <hamzii2205@gmail.com>
2024-12-19 02:06:11 +0500

    first commit

---
+++ src/sanity/schemaTypes/product.ts
@@ -0,0 +1,22 @@
+export const product = {
+  name: "product",
+  type: "document",
+  title: "product",
+  fields: [
+    {
+      name: "title",
+      title: " product Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Product description",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "product Image",
+      type: "image",
+    },
+  ],
+};
