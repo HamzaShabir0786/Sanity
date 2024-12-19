0d0b22d78c03bdb13fc361575b4c156932cb51c3
Hamza Shabir <hamzii2205@gmail.com>
2024-12-20 02:42:31 +0500

    add somedata

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,12 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  apiVersion: "v2024-12-18",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  token: process.env.SANITY_ACCESS_TOKEN,
+
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+});
